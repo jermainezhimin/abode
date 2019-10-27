92fb3e38965b4a51c1b45be0b8cadc1e1977aaf0
Jermaine Cheng <jermaine.zhimin@gmail.com>
2019-10-26 18:37:27 -0700

    Initializing repository

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React /* , { useState } */ from 'react';
+import { hot } from 'react-hot-loader/root';
+
+const App = () => {
+  return (
+    <>
+      <div>hi</div>
+      <h1 style={{ fontWeight: 'bold' }}>HELLO</h1>
+      <h1 style={{ fontFamily: "'Bodoni 72'" }}>moon</h1>
+    </>
+  );
+};
+
+export default hot(App);
